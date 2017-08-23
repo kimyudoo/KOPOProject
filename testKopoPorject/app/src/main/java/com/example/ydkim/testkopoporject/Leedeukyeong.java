9c54eb5a3edd67886a6913b9d2321751a6c6efd7
koposw17 <honolala@nate.com>
2017-08-23 16:05:23 +0900

    [20170823] class, xml update

--- testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/Leedeukyeong.java
+++ testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/Leedeukyeong.java
@@ -1,7 +1,10 @@
 package com.example.ydkim.testkopoporject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by 504ST-13 on 2017-08-23.
@@ -13,5 +16,27 @@ public class Leedeukyeong extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leedeukyeong);
+
+        Button btnBefore = (Button)findViewById(R.id.btnBefore);
+        btnBefore.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), kimjihyunActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+        Button btnNext = (Button)findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), namsgActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
