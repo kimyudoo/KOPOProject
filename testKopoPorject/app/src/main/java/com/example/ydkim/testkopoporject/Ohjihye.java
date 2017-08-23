e43fc286872dcc6354dcbcdef4dc83bc33e9275c
owl5678 <owl5678@naver.com>
2017-08-23 16:02:27 +0900

    [20170823] class, xml update

--- testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/Ohjihye.java
+++ testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/Ohjihye.java
@@ -1,7 +1,10 @@
 package com.example.ydkim.testkopoporject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by KOPOCTC on 2017-08-23.
@@ -13,5 +16,32 @@ public class Ohjihye extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ohjihye);
+
+        Button btnBefore = (Button)findViewById(R.id.btnBefore);
+        btnBefore.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), namsgActivity.class);
+                finish();
+                startActivity(intent);
+
+            }
+        });
+
+        Button btnFin = (Button)findViewById(R.id.btnFin);
+        btnBefore.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
 }
