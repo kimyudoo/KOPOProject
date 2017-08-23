57716b917aa8e557ac4054be885ee097ba1c8dc5
junwoo <ayng7942@daum.net>
2017-08-23 13:35:33 +0900

    [20170823] class update

---
+++ testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/junwooActivity.java
@@ -0,0 +1,16 @@
+package com.example.ydkim.testkopoporject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by s on 2017-08-23.
+ */
+
+public class junwooActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.junwoo);
+    }
+
+}
