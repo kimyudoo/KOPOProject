6311e9a43fe37e72374498815fed5d4cd1856fc6
namsg <nam393939@naver.com>
2017-08-23 13:52:31 +0900

    남상구

---
+++ testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/namsgActivity.java
@@ -0,0 +1,16 @@
+package com.example.ydkim.testkopoporject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 10 on 2017-08-23.
+ */
+
+public class namsgActivity  extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.namsg);
+    }
+
+}
