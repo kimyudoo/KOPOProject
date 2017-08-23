56078cfc1357583163eacdf7cb4550472b4e2519
dudehdi <dudehdi@gmail.com>
2017-08-23 13:29:00 +0900

    modified_by_ydc

---
+++ testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/choyoungdo_Activity.java
@@ -0,0 +1,12 @@
+package com.example.ydkim.testkopoporject;
+
+import android.app.Activity;
+
+/**
+ * Created by kopo on 2017-08-23.
+ */
+
+public class choyoungdo_Activity extends Activity{
+
+
+}
