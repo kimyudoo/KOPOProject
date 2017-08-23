4b262432a00cd4ed756c803f995598a2440effa8
YudooKim <kimyudoo@gmail.com>
2017-08-23 15:24:23 +0900

    Fix error

--- testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/dooooooo_Activity.java
+++ testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/dooooooo_Activity.java
@@ -1,5 +1,6 @@
 package com.example.ydkim.testkopoporject;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
@@ -10,25 +11,21 @@ import android.widget.TextView;
  * Created by admin on 2017-08-23.
  */
 
-public class dooooooo_Activity {
+public class dooooooo_Activity extends Activity {
 
-    public class MainActivity extends AppCompatActivity {
+    Animation flowAnim;
+    TextView dooooooo_txt;
 
-        Animation flowAnim;
-        TextView dooooooo_txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.dooooooo);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            //setContentView(R.layout.activity_main);
-            setContentView(R.layout.dooooooo);
+        dooooooo_txt = (TextView) findViewById(R.id.dooooooo_txt);
+        dooooooo_txt.setText("양두원입니다.");
+        flowAnim = AnimationUtils.loadAnimation(this, R.anim.flow);
 
-            dooooooo_txt = (TextView) findViewById(R.id.dooooooo_txt);
-            dooooooo_txt.setText("양두원입니다.");
-            flowAnim = AnimationUtils.loadAnimation(this, R.anim.flow);
-
-            dooooooo_txt.startAnimation(flowAnim);
-        }
+        dooooooo_txt.startAnimation(flowAnim);
     }
-
 }
