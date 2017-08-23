dc5a44e4bf54997ed99a3f63e4abefa47bff2944
MisterSGW <mistersgw@gmail.com>
2017-08-23 16:08:05 +0900

    [20170823] 손광원 버튼 업데이트

--- testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/GwangWon_Activity.java
+++ testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/GwangWon_Activity.java
@@ -1,11 +1,15 @@
 package com.example.ydkim.testkopoporject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by JK on 2017-08-23.
  */
+
 
 public class GwangWon_Activity extends Activity {
 
@@ -13,5 +17,21 @@ public class GwangWon_Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gwangwon);
+
+        Button GBACKbtn = (Button) findViewById(R.id.GBACKbtn);
+        GBACKbtn.setOnClickListener(new View.OnClickListener() {
+            public void  onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), dooooooo_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button GNEXTbtn = (Button) findViewById(R.id.GNEXTbtn);
+        GNEXTbtn.setOnClickListener(new View.OnClickListener() {
+            public void  onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), junwooActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
