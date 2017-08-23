7c1a28d3e139ff68d873c02bc753cd96635e44e9
YudooKim <kimyudoo@gmail.com>
2017-08-23 16:49:06 +0900

    MainActivity 버튼 추가

--- testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/MainActivity.java
+++ testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ydkim.testkopoporject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton;
+        startButton = (Button)findViewById(R.id.startBtn);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), choyoungdo_Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
