b52e28db38cf89bc2439695e70f6780366002176
namsg <nam393939@naver.com>
2017-08-23 15:41:42 +0900

    안뇽안뇽...

--- testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/namsgActivity.java
+++ testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/namsgActivity.java
@@ -1,16 +1,43 @@
 package com.example.ydkim.testkopoporject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by 10 on 2017-08-23.
  */
 
 public class namsgActivity  extends Activity {
+
+    private Button btnStart;
+    private Button btnStop;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.namsg);
+
+        Button front = (Button) findViewById(R.id.btnStart);
+        front.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(),
+                        Leedeukyeong.class);
+                startActivity(intent1);
+            }
+        });
+
+        Button back = (Button) findViewById(R.id.btnStop);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(),
+                        Ohjihye.class);
+                startActivity(intent1);
+            }
+        });
     }
 
 }
