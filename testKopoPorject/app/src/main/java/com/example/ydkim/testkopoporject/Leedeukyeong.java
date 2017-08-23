e7272d663931e66ea7cbd5b42bea0680694819f4
koposw17 <honolala@nate.com>
2017-08-23 13:45:53 +0900

    [20170823] class update

---
+++ testKopoPorject/app/src/main/java/com/example/ydkim/testkopoporject/Leedeukyeong.java
@@ -0,0 +1,17 @@
+package com.example.ydkim.testkopoporject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 504ST-13 on 2017-08-23.
+ */
+
+public class Leedeukyeong extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.leedeukyeong);
+    }
+}
